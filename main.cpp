b832fd8f4be4ad1fd1d70905b5317351e04b0f56
Alexander Kaess <alexanderkaess@MacBookPro.fritz.box>
2026-04-13 21:50:15 +0200

    add app icon

--- main.cpp
+++ main.cpp
@@ -13,15 +13,16 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    // add app icon
     QFileInfo info(__FILE__);
     QString parentPath = info.path();
     a.setWindowIcon(QIcon(parentPath + "/ui/AppIcon_small.png"));
 
-    // Konfiguration laden
+    // load configuration
     QString configPath = QCoreApplication::applicationDirPath() + "/log4cxx.xml";
     log4cxx::xml::DOMConfigurator::configure(configPath.toStdString());
 
-    // Logger erstellen
+    // create logger
     log4cxx::LoggerPtr logger{log4cxx::Logger::getLogger("HealthLogger.main")};
     LOG4CXX_INFO(logger, "----------");
     LOG4CXX_INFO(logger, "Program HealthAndProductivityAssistant started...");
