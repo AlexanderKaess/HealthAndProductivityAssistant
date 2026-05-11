6a81ea88e1d2b243d5ffd8a67f8fe669b2145f20
Alexander Kaess <kaessalexander@gmail.com>
2026-05-11 22:31:19 +0200

    add translation

--- resources/translations/HealthAndProductivityAssistant_de_DE.ts
+++ resources/translations/HealthAndProductivityAssistant_de_DE.ts
@@ -1,3 +1,55 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="de_DE"></TS>
+<TS version="2.1" language="de_DE">
+<context>
+	<name>MainWindow</name>
+	<message><source>Ready</source><translation>Bereit</translation></message>
+	<message><source>Timer beendet</source><translation>Timer beendet</translation></message>
+	<message><source>Timer activ</source><translation>Timer aktiv</translation></message>
+	<message><source>%1 timers still running. Really quit?</source>
+			 <translation>%1 Timer laufen noch. Wirklich beenden?</translation></message>
+	<message><source>pomodoro timer</source><translation>Pomodoro Timer</translation></message>
+	<message><source>stay hydrated reminder</source><translation>Trink-Erinnerung</translation></message>
+	<message><source>fresh air</source><translation>Lüften</translation></message>
+	<message><source>working hours tracker</source><translation>Arbeitszeit Tracker</translation></message>
+	<message><source>break</source><translation>Pause</translation></message>
+	<message><source>stand up & move</source><translation>Aufstehen und bewegen</translation></message>
+	<message><source>timer selection</source><translation>Timer Auswahl</translation></message>
+	<message><source>overview</source><translation>Übersicht</translation></message>
+	<message><source>settings</source><translation>Einstellungen</translation></message>
+	<message><source>select a timer</source><translation>Timer auswählen</translation></message>
+	<message><source>type</source><translation>Typ</translation></message>
+	<message><source>time remaining</source><translation>Verbleibende Zeit</translation></message>
+	<message><source>status</source><translation>Status</translation></message>
+	<message><source>task</source><translation>Aufgabe</translation></message>
+	<message><source>active</source><translation>Aktiv</translation></message>
+	<message><source>update</source><translation>Aktualisierung</translation></message>
+	<message><source>stop all</source><translation>Alle stoppen</translation></message>
+	<message><source>notifications</source><translation>Benachrichtigung</translation></message>
+	<message><source>volume</source><translation>Lautstärke</translation></message>
+	<message><source>play sound when timer ends</source>
+			<translation>Ton abspielen, wenn der Timer abgelaufen ist</translation></message>
+	<message><source>show popup notification</source>
+			<translation>Popup-Benachrichtigung anzeigen</translation></message>
+	<message><source>appearance</source><translation>Darstellung</translation></message>
+	<message><source>language</source><translation>Spache</translation></message>
+	<message><source>theme</source><translation>Theme</translation></message>
+	<message><source>behavior</source><translation>Verhalten</translation></message>
+	<message><source>confirm when closing a running timer</source>
+			<translation>Beim Schließen eines laufenden Timers bestätigen</translation></message>
+	<message><source>reset</source><translation>Reset</translation></message>
+	<message><source>save</source><translation>Speichern</translation></message>
+	<message><source>application settings</source><translation>Applikationseinstellunge</translation></message>
+	<message><source>Overview of current timers</source>
+			<translation>Übersicht aktueller Timer</translation></message>
+	<message><source>timer description</source><translation>Timerbeschreibung</translation></message>
+	<message><source>adjust time</source><translation>Zeit einstellen</translation></message>
+	<message><source>hours</source><translation>Stunden</translation></message>
+	<message><source>minutes</source><translation>Minuten</translation></message>
+	<message><source>seconds</source><translation>Sekunden</translation></message>
+	<message><source>start</source><translation>Start</translation></message>
+	<message><source>stop</source><translation>Stop</translation></message>
+	<message><source>reset</source><translation>Reset</translation></message>
+	<message><source>close</source><translation>Schließen</translation></message>
+</context>
+</TS>
--- resources/translations/HealthAndProductivityAssistant_en_GB.ts
+++ resources/translations/HealthAndProductivityAssistant_en_GB.ts
@@ -1,3 +1,55 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_GB"></TS>
+<TS version="2.1" language="en_GB">
+<context>
+    <name>MainWindow</name>
+    <message><source>Ready</source><translation>Ready</translation></message>
+    <message><source>Timer stopped</source><translation>Timer finished</translation></message>
+    <message><source>Timer active</source><translation>Timer active</translation></message>
+    <message><source>%1 timers still running. Really quit?</source>
+             <translation>%1 timers still running. Really quit?</translation></message>
+    <message><source>pomodoro timer</source><translation>pomodoro timer</translation></message>
+    <message><source>stay hydrated reminder</source><translation>stay hydrated reminder</translation></message>
+    <message><source>fresh air</source><translation>fresh air</translation></message>
+    <message><source>working hours tracker</source><translation>working hours tracker</translation></message>
+    <message><source>break</source><translation>break</translation></message>
+    <message><source>stand up & move</source><translation>stand up & move</translation></message>
+    <message><source>timer selection</source><translation>timer selection</translation></message>
+    <message><source>overview</source><translation>timer selection</translation></message>
+    <message><source>settings</source><translation>settings</translation></message>
+    <message><source>select a timer</source><translation>select a timer</translation></message>
+    <message><source>type</source><translation>type</translation></message>
+    <message><source>time remaining</source><translation>time remaining</translation></message>
+    <message><source>status</source><translation>status</translation></message>
+    <message><source>task</source><translation>task</translation></message>
+    <message><source>active</source><translation>active</translation></message>
+    <message><source>update</source><translation>update</translation></message>
+    <message><source>stop all</source><translation>stop all</translation></message>
+    <message><source>notifications</source><translation>notifications</translation></message>
+    <message><source>volume</source><translation>volume</translation></message>
+    <message><source>play sound when timer ends</source>
+            <translation>play sound when timer ends</translation></message>
+    <message><source>show popup notification</source>
+            <translation>show popup notification</translation></message>
+    <message><source>appearance</source><translation>appearance</translation></message>
+    <message><source>language</source><translation>language</translation></message>
+    <message><source>theme</source><translation>theme</translation></message>
+    <message><source>behavior</source><translation>behavior</translation></message>
+    <message><source>confirm when closing a running timer</source>
+            <translation>confirm when closing a running timer</translation></message>
+    <message><source>reset</source><translation>reset</translation></message>
+    <message><source>save</source><translation>save</translation></message>
+    <message><source>application settings</source><translation>application settings</translation></message>
+    <message><source>Overview of current timers</source>
+            <translation>Overview of current timers</translation></message>
+    <message><source>timer description</source><translation>timer description</translation></message>
+    <message><source>adjust time</source><translation>adjust time</translation></message>
+    <message><source>hours</source><translation>hours</translation></message>
+    <message><source>minutes</source><translation>minutes</translation></message>
+    <message><source>seconds</source><translation>seconds</translation></message>
+    <message><source>start</source><translation>start</translation></message>
+    <message><source>stop</source><translation>stop</translation></message>
+    <message><source>reset</source><translation>reset</translation></message>
+    <message><source>close</source><translation>close</translation></message>
+</context>
+</TS>
